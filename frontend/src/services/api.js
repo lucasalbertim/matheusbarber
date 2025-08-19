e2d54bb5e9378529b6303a514b75a1d3b33d76f9
jamerson-mt <jjamersonmt@gmail.com>
2025-08-19 19:09:00 -0300

    ok

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://172.21.201.15:8000',
+  baseURL: process.env.REACT_APP_API_URL || 'http://191.252.195.85/api/',
   timeout: 10000,
 });
 
